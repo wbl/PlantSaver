2bca6b0c075f782255380adcf9bb959cb6ef0bae
Watson Ladd <watsonbladd@gmail.com>
2012-02-06 11:15:56 -0600

    Makes pictures. Now to clean up and add more

---
+++ Main.hs
@@ -0,0 +1,19 @@
+module Main where
+import qualified Data.Map as Map
+import Turtle
+import Lindmayer
+import Graphics.Gloss
+koch = System (Symbol "F") (Map.fromList[(Symbol "F",
+                                                    [Symbol "F",
+                                                     Symbol "L",
+                                                     Symbol "F",
+                                                     Symbol "R",
+                                                     Symbol "F",
+                                                     Symbol "R",
+                                                     Symbol "F",
+                                                     Symbol "L",
+                                                     Symbol "F"])])
+       (Map.fromList[(Symbol "F", Forward 5), (Symbol "R", TurnRight 90),
+                                             (Symbol "L", TurnLeft 90)])
+                                                     
+main = display (InWindow "Nice Window" (1000, 1000) (100, 100)) white (fractal 5 koch)
