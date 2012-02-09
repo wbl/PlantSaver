c8d680a4f1257a0c7be3ede13ded238e3fe5fa74
Watson Ladd <watsonbladd@gmail.com>
2012-02-09 11:38:08 -0600

    More fractals

--- Main.hs
+++ Main.hs
@@ -15,6 +15,44 @@ koch = System (Symbol "F") (Map.fromList[(Symbol "F",
                                                      Symbol "F"])])
        (Map.fromList[(Symbol "F", Forward 5), (Symbol "R", TurnRight 90),
                                              (Symbol "L", TurnLeft 90)])
-                                                     
-main = animate (FullScreen (1000,1000)) white
-       (\x->(Translate (-500)  (200) (fractal (min (floor(x/2)) 5) koch)))
+gosper = System (Symbol "FL") (Map.fromList[
+                                       ( Symbol "FL",
+                                                     [ Symbol "FL",
+                                                       Symbol "+",
+                                                       Symbol "FR",
+                                                       Symbol "+",
+                                                       Symbol "+",
+                                                       Symbol "FR",
+                                                       Symbol "-",
+                                                       Symbol "FL",
+                                                       Symbol "-",
+                                                       Symbol "-",
+                                                       Symbol "FL",
+                                                       Symbol "FL",
+                                                       Symbol "-",
+                                                       Symbol "FR",
+                                                       Symbol "+"]),
+                                       (Symbol "FR", 
+                                                   [ Symbol "-",
+                                                     Symbol "FL",
+                                                     Symbol "+",
+                                                     Symbol "FR",
+                                                     Symbol "FR",
+                                                     Symbol "+",
+                                                     Symbol "+",
+                                                     Symbol "FR",
+                                                     Symbol "+",
+                                                     Symbol "FL",
+                                                     Symbol "-",
+                                                     Symbol "-",
+                                                     Symbol "FL",
+                                                     Symbol "-",
+                                                     Symbol "FR"])])
+         (Map.fromList [(Symbol "FR", Forward 5), (Symbol "FL", Forward 5),
+                                                 (Symbol "-", TurnLeft 60),
+                                                 (Symbol "+", TurnRight 60)])
+                                                         
+fractfuncs = [ (\x->(Translate (-500)  (200)
+                                   (fractal (min (floor(x/2)) 5) koch))),
+               (\x->(fractal (min (floor(x/2)) 6) gosper))] 
+main = animate (FullScreen (1000,1000)) white (fractfuncs !! 1)
